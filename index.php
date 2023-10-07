6012a55de1df0a95e72be249494455bb68361aaf
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2023-10-07 22:02:39 +0600

    new changes

--- index.php
+++ index.php
@@ -19,38 +19,9 @@
 
 <body>
   <!-- Start Header/Navigation -->
-  <nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
-    <div class="container">
-      <a class="navbar-brand" href="index.html">Furni<span>.</span></a>
-
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarsFurni">
-        <ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
-          <li class="nav-item active">
-            <a class="nav-link" href="index.html">Home</a>
-          </li>
-          <li><a class="nav-link" href="shop.html">Shop</a></li>
-          <li><a class="nav-link" href="about.html">About us</a></li>
-          <li><a class="nav-link" href="services.html">Services</a></li>
-          <li><a class="nav-link" href="blog.html">Blog</a></li>
-          <li><a class="nav-link" href="contact.html">Contact us</a></li>
-        </ul>
-
-        <ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
-          <li>
-            <a class="nav-link" href="#"><img src="images/user.svg" /></a>
-          </li>
-          <li>
-            <a class="nav-link" href="cart.html"><img src="images/cart.svg" /></a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <!-- End Header/Navigation -->
+  <?php
+  include "shared/navbar.php"
+  ?>
 
   <!-- Start Hero Section -->
   <div class="hero">
@@ -483,127 +454,9 @@
   <!-- End Blog Section -->
 
   <!-- Start Footer Section -->
-  <footer class="footer-section">
-    <div class="container relative">
-      <div class="sofa-img">
-        <img src="images/sofa.png" alt="Image" class="img-fluid" />
-      </div>
-
-      <div class="row">
-        <div class="col-lg-8">
-          <div class="subscription-form">
-            <h3 class="d-flex align-items-center">
-              <span class="me-1"><img src="images/envelope-outline.svg" alt="Image" class="img-fluid" /></span><span>Subscribe to Newsletter</span>
-            </h3>
-
-            <form action="#" class="row g-3">
-              <div class="col-auto">
-                <input type="text" class="form-control" placeholder="Enter your name" />
-              </div>
-              <div class="col-auto">
-                <input type="email" class="form-control" placeholder="Enter your email" />
-              </div>
-              <div class="col-auto">
-                <button class="btn btn-primary">
-                  <span class="fa fa-paper-plane"></span>
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-
-      <div class="row g-5 mb-5">
-        <div class="col-lg-4">
-          <div class="mb-4 footer-logo-wrap">
-            <a href="#" class="footer-logo">Furni<span>.</span></a>
-          </div>
-          <p class="mb-4">
-            Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio
-            quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam
-            vulputate velit imperdiet dolor tempor tristique. Pellentesque
-            habitant
-          </p>
-
-          <ul class="list-unstyled custom-social">
-            <li>
-              <a href="#"><span class="fa fa-brands fa-facebook-f"></span></a>
-            </li>
-            <li>
-              <a href="#"><span class="fa fa-brands fa-twitter"></span></a>
-            </li>
-            <li>
-              <a href="#"><span class="fa fa-brands fa-instagram"></span></a>
-            </li>
-            <li>
-              <a href="#"><span class="fa fa-brands fa-linkedin"></span></a>
-            </li>
-          </ul>
-        </div>
-
-        <div class="col-lg-8">
-          <div class="row links-wrap">
-            <div class="col-6 col-sm-6 col-md-3">
-              <ul class="list-unstyled">
-                <li><a href="#">About us</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">Contact us</a></li>
-              </ul>
-            </div>
-
-            <div class="col-6 col-sm-6 col-md-3">
-              <ul class="list-unstyled">
-                <li><a href="#">Support</a></li>
-                <li><a href="#">Knowledge base</a></li>
-                <li><a href="#">Live chat</a></li>
-              </ul>
-            </div>
-
-            <div class="col-6 col-sm-6 col-md-3">
-              <ul class="list-unstyled">
-                <li><a href="#">Jobs</a></li>
-                <li><a href="#">Our team</a></li>
-                <li><a href="#">Leadership</a></li>
-                <li><a href="#">Privacy Policy</a></li>
-              </ul>
-            </div>
-
-            <div class="col-6 col-sm-6 col-md-3">
-              <ul class="list-unstyled">
-                <li><a href="#">Nordic Chair</a></li>
-                <li><a href="#">Kruzo Aero</a></li>
-                <li><a href="#">Ergonomic Chair</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="border-top copyright">
-        <div class="row pt-4">
-          <div class="col-lg-6">
-            <p class="mb-2 text-center text-lg-start">
-              Copyright &copy;
-              <script>
-                document.write(new Date().getFullYear());
-              </script>
-              . All Rights Reserved. &mdash; Designed with love by
-              <a href="https://untree.co">Untree.co</a> Distributed By
-              <a hreff="https://themewagon.com">ThemeWagon</a>
-            </p>
-          </div>
-
-          <div class="col-lg-6 text-center text-lg-end">
-            <ul class="list-unstyled d-inline-flex ms-auto">
-              <li class="me-4"><a href="#">Terms &amp; Conditions</a></li>
-              <li><a href="#">Privacy Policy</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
+  <?php
+  include "shared/footer.php";
+  ?>
 
   <script src="js/bootstrap.bundle.min.js"></script>
   <script src="js/tiny-slider.js"></script>
